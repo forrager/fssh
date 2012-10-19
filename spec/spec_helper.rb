29fbad90730cb38ec2433d13a0fd6d46a509f706
Cameron Dutro <cdutro@twitter.com>
2012-10-19 11:14:53 -0700

    Added spec_helper, no actual tests yet

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+# Copyright 2012 Twitter, Inc
+# http://www.apache.org/licenses/LICENSE-2.0
+
+require 'rspec'
+require 'fssh'
+
+RSpec.configure do |config|
+  config.mock_with :rr
+end
